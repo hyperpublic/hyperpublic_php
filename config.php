0d5e2b0f1d364259d048ae0b87f366b2b185ec61
Jonathan Vingiano <jvingiano@gmail.com>
2011-06-07 15:43:30 -0700

    setting keys to empty strings

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ if (preg_match("/config\.php$/", $_SERVER['PHP_SELF'])){
 	exit('No direct script access allowed');
 }
 
-define('CONSUMER_KEY', 'your key');
-define('CONSUMER_SECRET', 'super secret');
+define('CONSUMER_KEY', '');
+define('CONSUMER_SECRET', '');
 
 ?>
